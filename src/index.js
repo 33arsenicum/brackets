55ceb8b841792f531aac3b50304bd0e43dacce4a
33arsenicum <cmykbielaruski@gmail.com>
2023-08-10 23:18:50 +0300

    feat: the task is solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+// module.exports = function check(str, bracketsConfig) {
+//   your solution
+// }
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const bracketsMatch = {};
+
+  for (const [open, close] of bracketsConfig) {
+    bracketsMatch[close] = open;
+  }
+
+  for (const char of str) {
+    if (char in bracketsMatch) {
+      if (stack.length === 0 || stack[stack.length - 1] !== bracketsMatch[char]) {
+        stack.push(char);
+      } else {
+        stack.pop();
+      }
+    } else {
+      stack.push(char);
+    }
+  }
+
+  return stack.length === 0;
+};
+
